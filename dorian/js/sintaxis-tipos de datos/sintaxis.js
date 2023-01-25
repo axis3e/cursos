ee6517dfda8392eea05ec5cd0b00377efaf9b31b
axis3e <barielhdz26@gmail.com>
2023-01-25 12:35:28 -0600

    Agregando Carpeta JS

---
+++ dorian/js/sintaxis-tipos de datos/sintaxis.js
@@ -0,0 +1,10 @@
+let numero = 5555555;
+let palabra = "Soy una variable";
+let string = "Soy un string"
+const constante = "Soy una cosntante"
+palabra = "palabrerias"
+
+console.log(numero);
+console.log(palabra);
+console.log(string);
+console.log(constante);
