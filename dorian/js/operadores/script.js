bdcc5f6d1ddbf343d2e694394bcb712f3fba9d1a
axis3e <barielhdz26@gmail.com>
2023-01-26 23:32:11 -0600

    Segunda clase js

---
+++ dorian/js/operadores/script.js
@@ -0,0 +1,54 @@
+/*Operadores matematicos*/ 
+let a = 5;
+let b = 6;
+console.log(a+b)
+
+a = 4;
+b = 2;
+console.log(a-b)
+
+a = 8;
+b = 7;
+console.log(a*b)
+
+a = 9;
+b = 3;
+console.log(a/b)
+
+let user = "axis"
+console.log("que me ves pinche " + user)
+
+
+/* Operadores de asignación */
+let ab = 10;
+
+ab += 5;
+console.log(ab)
+
+ab -= 3;
+console.log(ab)
+
+ab *= 2;
+console.log(ab)
+
+ab /= 3;
+console.log(ab)
+
+ab %= 3;
+console.log(ab)
+
+
+/* Operadores de incremetnto/decremento */
+let c = 10;
+let d = 5;
+console.log(c++)
+console.log(c)
+console.log(++d)
+console.log(d)
+
+let e = 12;
+let f = 15;
+console.log(e--)
+console.log(e)
+console.log(--f)
+console.log(f)
